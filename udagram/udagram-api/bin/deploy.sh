ed83e8d391cddaed6a9109ed2732e504c1f96545
Nour-Eldeen <noureldeen.nasser@tmentors.com>
2023-02-02 13:04:29 +0200

    add bin folder

---
+++ udagram/udagram-api/bin/deploy.sh
@@ -0,0 +1,13 @@
+eb use udagram-api-dev
+
+POSTGRES_USERNAME=postgres
+AWS_ACCESS_KEY_ID=AKIAXQQR3TJKXY7GZTM6
+AWS_SECRET_ACCESS_KEY=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
+JWT_SECRET=hey-iam-NourEldeen
+AWS_BUCKET=udagrambucket
+AWS_PROFILE=default
+POSTGRES_PASSWORD=22422
+POSTGRES_DB=database-1
+POSTGRES_HOST=database-1.cfosgp7ideui.us-east-1.rds.amazonaws.com
+AWS_REGION=us-east-1d
+URL=udagram-api-dev.eba-pekhjbht.us-east-1.elasticbeanstalk.com
